233cec2c9eaff6731b3e5ed9f31d2eccfa636fa8
Sunriver <seriousman1994217winter@gmail.com>
2022-03-30 09:55:16 +0800

    Update fonts and colours

--- components/TheLogo.vue
+++ components/TheLogo.vue
@@ -1,9 +1,16 @@
 <template>
-	<a href="/" class="logo" title="Click to visit the dashboard">Nuon</a>
+	<a href="/" class="logo" title="Click to visit the dashboard">
+		<NuonLogo />
+	</a>
 </template>
 
 <script>
+import NuonLogo from "@/assets/images/logo/logo-nuon.svg";
+
 export default {
 	name: "TheLogo",
+	components: {
+		NuonLogo
+	},
 };
 </script>
--- components/TheWallet.vue
+++ components/TheWallet.vue
@@ -6,46 +6,37 @@
 			@click="connect">
 			Connect Wallet
 		</TheButton>
-		<div v-else class="wallet__details">
-			<Jazzicon
-				:address="connectedAccount"
-				:diameter="28" />
-			<TheButton
-				title="Click to disconnect wallet"
-				@click="disconnect">
-				<span>{{ shortAddress(connectedAccount) }}</span>
-			</TheButton>
-		</div>
+		<TheButton
+			v-else
+			title="Click to disconnect wallet"
+			@click="disconnect">
+			<span>{{ shortAddress(connectedAccount) }}</span>
+		</TheButton>
 	</div>
 </template>
 
 <script>
-import Jazzicon from "vue-jazzicon";
-
 export default {
 	name: "TheWallet",
-	components: {
-		[Jazzicon.name]: Jazzicon,
-	},
 	data () {
 		return {
 			web3: null,
 		};
 	},
-
 	computed: {
 		balance() {
 			return this.$store.state.web3Store.balance;
 		}
 	},
-
 	mounted () {
 		this.$store.dispatch("web3Store/init");
 	},
-
 	methods: {
 		connect () {
-			this.$store.commit("modalStore/setModalVisibility", {name: "connectWalletModal", visibility: true});
+			this.$store.commit("modalStore/setModalVisibility", {
+				name: "connectWalletModal",
+				visibility: true
+			});
 		},
 		disconnect () {
 			this.$store.dispatch("web3Store/disconnect");
