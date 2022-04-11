310d2b3c3bc181660a8972f8fcec212031e90804
Sunriver <seriousman1994217winter@gmail.com>
2022-04-11 16:32:42 +0800

    Transaction input updates

--- components/TransactionSummary.vue
+++ components/TransactionSummary.vue
@@ -1,16 +1,16 @@
 <template>
 	<div>
-		<h4 class="u-mb-xs">Transaction Summary</h4>
+		<h5 class="u-mb-12 u-color-dark-grey">Transaction Summary</h5>
 		<div class="transaction-summary">
 			<LayoutFlex
 				v-for="(value, index) in values"
 				:key="index"
 				direction="row-start-space-between"
-				:class="`${index !== values.length - 1 && 'u-mb-xs'} ${index === values.length - 2 && 'u-pb-xs u-bb-black'}`">
+				:class="`${index !== values.length - 1 && 'u-mb-8'} ${index === values.length - 2 && 'u-pb-8 u-bb-black'}`">
 				<p>{{ value.title }}</p>
 				<LayoutFlex direction="column-end">
 					<p>{{ value.val }} {{ value.currency && value.currency }}</p>
-					<p class="u-colour-grey-dark">~ ${{ value.dollar }}</p>
+					<p>~ ${{ value.dollar }}</p>
 				</LayoutFlex>
 			</LayoutFlex>
 		</div>
