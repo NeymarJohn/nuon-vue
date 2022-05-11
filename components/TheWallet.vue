2122e06f10a1c678346263cbe41b7dca5bd84dfc
Sunriver <seriousman1994217winter@gmail.com>
2022-05-11 13:46:05 +0800

    Boilerplate layout

--- components/TheWallet.vue
+++ components/TheWallet.vue
@@ -3,12 +3,14 @@
 		<TheButton
 			v-if="!isConnectedWallet"
 			title="Click to connect wallet"
+			size="lg"
 			@click="connect">
 			Connect Wallet
 		</TheButton>
 		<TheButton
 			v-else
 			title="Click to disconnect wallet"
+			size="lg"
 			@click="disconnect">
 			<span>{{ shortAddress(connectedAccount) }}</span>
 		</TheButton>
--- pages/index.vue
+++ pages/index.vue
@@ -5,67 +5,31 @@
 				<h4>Dashboard</h4>
 				<h1>Nuon Protocol Information</h1>
 			</PageTitle>
-			<NuxtLink class="btn btn--md" to="/my-page" title="Click to view my page">My Page</NuxtLink>
+			<NuxtLink class="btn btn--lg btn--user" to="/my-page" title="Click to view my page">
+				<UserIcon />
+				My Page
+			</NuxtLink>
 		</LayoutFlex>
-		<h2>Account Balance</h2>
+		<h2 class="u-mb-24">Vault Overview</h2>
+		<div class="l-chart">
+			<div class="chart"></div>
+			<div class="chart"></div>
+		</div>
 	</LayoutContainer>
 </template>
 
 <script>
-import { HX, USX } from "~/constants/tokens";
+import UserIcon from "@/assets/images/svg/svg-user.svg";
+
 export default {
 	name: "TheDashboard",
-	data () {
-		return {
-			items: [
-				{
-					label: "Peg range",
-					statistic: "5% around the peg"
-				},
-				{
-					label: "HX minted last year",
-					statistic: "8,302"
-				},
-				{
-					label: "USX volatility last year",
-					statistic: "2%"
-				},
-				{
-					label: "HX burnt last year",
-					statistic: "10,479"
-				},
-				{
-					label: "USX time in peg range",
-					statistic: "97%"
-				},
-				{
-					label: "Average APY on vested HX",
-					statistic: "917%"
-				},
-				{
-					label: "Average ROI by stability arbitragers",
-					statistic: "369%"
-				}
-			]
-		};
+	components: {
+		UserIcon,
 	},
 	head () {
 		return {
 			title: "Dashboard | Nuon"
 		};
 	},
-	computed: {
-		usxPrice() {
-			return this.tokenPrices[USX.symbol]; 
-		},
-		hxPrice() {
-			return this.tokenPrices[HX.symbol];
-		}
-	},
-	mounted () {
-		this.$store.commit("rootStore/setIsLoaded", true);
-		this.$store.commit("rootStore/setInfuraId", this.$config.infuraId);
-	},
-
 };
 </script>
