159b4c212fe6c5a9c2b8f509949ea556d6cf2bb6
Sunriver <seriousman1994217winter@gmail.com>
2022-06-15 16:47:03 +0800

    mod: mv if statements in TheNotification to computed

--- components/TheNotification.vue
+++ components/TheNotification.vue
@@ -1,8 +1,6 @@
 <template>
 	<div class="notification">
-		<p v-if="myCollateralizationRatio > minCollateralizationRatio">Your collateralization ratio is good and at low risk of liquidation.</p>
-		<p v-if="myCollateralizationRatio && myCollateralizationRatio <= minCollateralizationRatio">Your collateralization ratio is low and at high risk of liquidation. Please deposit more collateral.</p>
-		<p v-if="myCollateralizationRatio === 0">Hi there, please deposit your collaterals to mint NUON.</p>
+		<p>{{ message }}</p>
 	</div>
 </template>
 
@@ -18,6 +16,14 @@ export default {
 			type: [Number, String],
 			required: true
 		},
+	},
+	computed: {
+		message() {
+			if (this.myCollateralizationRatio && this.myCollateralizationRatio > this.minCollateralizationRatio) return "Your collateralization ratio is good and at low risk of liquidation.";
+			if (this.myCollateralizationRatio && this.myCollateralizationRatio <= this.minCollateralizationRatio) return "Your collateralization ratio is low and at high risk of liquidation. Please deposit more collateral.";
+			if (this.myCollateralizationRatio && this.myCollateralizationRatio === 0) return "Hi there, please deposit your collaterals to mint NUON.";
+			return "";
+		}
 	}
 };
 </script>
