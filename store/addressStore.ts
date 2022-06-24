e4d254fdaae993c3546413c2a794324f0972488d
Sunriver <seriousman1994217winter@gmail.com>
2022-06-24 18:54:31 +0800

    mod: changes for rinkeby support

--- store/addressStore.ts
+++ store/addressStore.ts
@@ -19,6 +19,18 @@ export const state = () => ({
 			treasury: TREASURY_ADDRESS,
 			uniswapV2Pair: tokenPairs
 		},
+		4: {
+			boardroom: "0x0a5e2EeA70B9Db9f801750feEcA9b245aBC7a388",
+			router: ROUTER_ADDRESS,
+			nuonController: "0x0AF39204257a41c7B09d8AF8993dc5D03141a1CA",
+			oracle: "0x9649329FbaBF1b49b3c1d8E04ea1A1978d691521",
+			weth: "0x1AB158d908aCFD0D4FdD0082675c411a203ECAB6",
+			hydro: "0xB02322B6Cf0c8C25Ebd0Cad4aDC8ab935413a177",
+			nuon: "0xE66Edfc2F5B2E98A35074eeAd30f5B6Ed2cccE4E",
+			usdc: "0xBD9c0bc5AeB998A9d6C4e8293d2A49111EdE5509",
+			treasury: "0xD3902ce5C4b4c4bdC7Fb5E7481b2B70fA1e4aB75",
+			uniswapV2Pair: tokenPairs
+		}
 	},
 	tokens: {
 		ETH: WETH_ADDRESS,
