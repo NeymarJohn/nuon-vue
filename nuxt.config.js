f589c18ebff70abdcd4463efd1cee1837e3f2e77
Sunriver <seriousman1994217winter@gmail.com>
2022-09-21 14:09:50 +0800

    Update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,13 +15,15 @@ export default {
 			{ charset: "utf-8" },
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
 			{ hid: "description", name: "description", content: "" },
-			{ name: "format-detection", content: "telephone=no" }
+			{ name: "format-detection", content: "telephone=no" },
+			{ name: "msapplication-TileColor", content: "#18181a" },
+			{ name: "theme-color", content: "#18181a" },
 		],
 		link: [
 			{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
 			{ rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
-			{ rel: "icon", sizes: "32x32", href: "/favicon-32x32.png" },
-			{ rel: "icon", sizes: "16x16", href: "/favicon-16x16.png" },
+			{ rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+			{ rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
 			{ rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
 			{ rel: "manifest", href: "/site.webmanifest" }
 		]
